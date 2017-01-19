afe99658dfc7e63b21734f0d647038b037844c6f
martincz <martincz@mokeedev.com>
2017-01-19 14:50:36 +0800

    klteduos: mokee-ify

--- lineage.mk
+++
@@ -1,6 +0,0 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/klteduos/full_klteduos.mk)
-
-PRODUCT_DEVICE := klteduos
-PRODUCT_NAME := lineage_klteduos
---
+++ mokee.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/klteduos/full_klteduos.mk)
+
+PRODUCT_DEVICE := klteduos
+PRODUCT_NAME := mk_klteduos
